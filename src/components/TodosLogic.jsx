18533a095fc881ffd3596b917dcf8a49dd48382d
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-02-27 15:47:45 -0400

    Add and delete task funcionality

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import InputTodo from "@/components/InputTodo";
 import TodosList from "@/components/TodosList";
 
 const TodosLogic = () => {
-  const todos = [
+  const [todos, setTodos] = useState([
     {
       id: 1,
       title: "Setup development environment",
@@ -18,12 +19,43 @@ const TodosLogic = () => {
       title: "Deploy to live server",
       completed: false,
     },
-  ];
+  ]);
+
+  const handleChange = (id) => {
+    setTodos((prevState) =>
+      prevState.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: 4,
+      title: title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const delTodo = (id) => {
+    setTodos([
+      ...todos.filter((todo) => {
+        return todo.id !== id;
+      }),
+    ]);
+  };
 
   return (
     <div>
-      <InputTodo />
-      <TodosList todosProps={todos} />
+      <InputTodo addTodoItem={addTodoItem} />
+      <TodosList todosProps={todos} handleChange={handleChange} delTodo={delTodo} />
     </div>
   );
 };
